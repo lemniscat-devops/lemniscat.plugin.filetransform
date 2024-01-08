5b47eb43b60676008d72b732982c73441570d3da
Philippe MORISSEAU <philippe.morisseau@outlook.com>
2024-01-08 11:20:20 +0100

    feat: initialize

---
+++ src/lemniscat/plugin/filetransform/filetransform.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+# above is for compatibility of python2.7.11
+
+import logging
+import os
+import subprocess, sys   
+from lemniscat.core.util.helpers import LogUtil
+import re
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.setLoggerClass(LogUtil)
+log = logging.getLogger(__name__.replace('lemniscat.', ''))
+
+class FileTransform:
+    def __init__(self):
+        pass
+    
+    # parse yaml file to dict
+    @staticmethod
+    def parseYamlFile(filePath) -> dict:
+        import yaml
+        with open(filePath, 'r') as stream:
+            try:
+                return yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                log.error(exc)
+                return None
+            
+    # parse json file to dict
+    @staticmethod
+    def parseJsonFile(filePath) -> dict:
+        import json
+        with open(filePath, 'r') as stream:
+            try:
+                return json.load(stream)
+            except json.JSONDecodeError as exc:
+                log.error(exc)
+                return None
+            
+    # save dict to yaml file
+    @staticmethod
+    def saveYamlFile(filePath, data: dict) -> None:
+        import yaml
+        with open(filePath, 'w') as stream:
+            try:
+                yaml.dump(data, stream)
+            except yaml.YAMLError as exc:
+                log.error(exc)
+
+    # save dict to json file
+    @staticmethod
+    def saveJsonFile(filePath, data: dict) -> None:
+        import json
+        with open(filePath, 'w') as stream:
+            try:
+                json.dump(data, stream)
+            except json.JSONDecodeError as exc:
+                log.error(exc)
+                
+    # get files path match pattern in directory
+    @staticmethod
+    def getFilesPathMatchPattern(directory, pattern) -> list:
+        import glob
+        return glob.glob(f'{directory}/{pattern}')
+
+    # replace variable in dict
+    @staticmethod
+    def replaceVariable(data: dict, key: str, value: str, prefix: str = '') -> dict:
+        for k, v in data.items():
+            if(f'{prefix}{k}'.casefold() == key.casefold()):
+                log.debug(f'Found {key}. Replace {key}...')
+                data[k] = value
+            else:
+                if(isinstance(v, dict) and key.casefold().startswith(f'{prefix}{k}'.casefold()) ):
+                    data[k] = FileTransform.replaceVariable(v.copy(), key, value, f'{prefix}{k}.')
+        return data
+
+    def run(self, folderPath: str, targetFiles: str, fileType: str, variables: dict = {}) -> None:
+        # get all files path match pattern
+        files = self.getFilesPathMatchPattern(folderPath, targetFiles)
+        # loop files
+        for file in files:
+            # parse file
+            if(fileType == 'yaml'):
+                data = self.parseYamlFile(file)
+            elif(fileType == 'json'):
+                data = self.parseJsonFile(file)
+            else:
+                log.error('File type not supported')
+                return 1, '','File type not supported'
+            # replace variables
+            for key, value in variables.items():
+                data = self.replaceVariable(data, key, value)
+            # save file
+            if(fileType == 'yaml'):
+                self.saveYamlFile(file, data)
+            elif(fileType == 'json'):
+                self.saveJsonFile(file, data)
+            else:
+                log.error('File type not supported')
+                return 1, '','File type not supported'
+        return 0, '',''
---
+++ src/lemniscat/plugin/filetransform/main.py
@@ -0,0 +1,73 @@
+
+import argparse
+import ast
+import logging
+import os
+from logging import Logger
+from lemniscat.core.contract.engine_contract import PluginCore
+from lemniscat.core.model.models import Meta, TaskResult
+from lemniscat.core.util.helpers import FileSystem, LogUtil
+
+from lemniscat.plugin.filetransform.filetransform import FileTransform
+
+
+class Action(PluginCore):
+
+    def __init__(self, logger: Logger) -> None:
+        super().__init__(logger)
+        plugin_def_path = os.path.abspath(os.path.dirname(__file__)) + '/plugin.yaml'
+        manifest_data = FileSystem.load_configuration_path(plugin_def_path)
+        self.meta = Meta(
+            name=manifest_data['name'],
+            description=manifest_data['description'],
+            version=manifest_data['version']
+        )
+
+    def __run_filetransform(self, parameters: dict = {}, variables: dict = {}) -> TaskResult:
+        # launch powershell command
+        filetransform = FileTransform()     
+        result = filetransform.run(parameters['folderPath'], parameters['targetFiles'], parameters['fileType'], variables)
+                
+        if(result[0] != 0):
+            return TaskResult(
+                name=f'FileTransform run',
+                status='Failed',
+                errors=result[2])
+        else:
+            return TaskResult(
+                name='FileTransform run',
+                status='Completed',
+                errors=[0x0000]
+        )
+        
+
+    def invoke(self, parameters: dict = {}, variables: dict = {}) -> TaskResult:
+        self._logger.debug(f'Transform file for {parameters["fileType"]} -> {self.meta}')
+        task = self.__run_filetransform(parameters, variables)
+        return task
+    
+    def test_logger(self) -> None:
+        self._logger.debug('Debug message')
+        self._logger.info('Info message')
+        self._logger.warning('Warning message')
+        self._logger.error('Error message')
+        self._logger.critical('Critical message')
+
+def __init_cli() -> argparse:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p', '--parameters', required=True, 
+        help="""(Required) Supply a dictionary of parameters which should be used. The default is {}
+        """
+    )
+    parser.add_argument(
+        '-v', '--variables', required=True, help="""(Optional) Supply a dictionary of variables which should be used. The default is {}
+        """
+    )                
+    return parser
+        
+if __name__ == "__main__":
+    logger = LogUtil.create()
+    action = Action(logger)
+    __cli_args = __init_cli().parse_args()   
+    action.invoke(ast.literal_eval(__cli_args.parameters), ast.literal_eval(__cli_args.variables))
